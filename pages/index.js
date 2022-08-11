689a067ea84c009a0be4e34091c1bff3e2f99503
mdmarufsarker <mdmarufsarker2021@gmail.com>
2022-08-11 09:12:55 +0600

    next tailwind starter

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className="
+     bg-indigo-600 py-96"
+    >
+      <h1 className="underline text-3xl text-center text-white">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
